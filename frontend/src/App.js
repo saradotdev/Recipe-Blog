64afea950a13e5d5a2cebe6b2bd31c0e773c4986
saraazizhere <saraaziz180903@gmail.com>
2025-04-03 01:39:46 +0500

    complete project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import Navbar from "./components/navbar/Navbar";
+import Navbar from "./components/navbar/Navbar.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./pages/main/Main";
 import Details from "./pages/details/Details";
